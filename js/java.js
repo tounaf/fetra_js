975a7c5cd5382b82b126128bf338576c0616b6df
fetra <harinjatovo.fetra@gmail.com>
2018-04-06 15:04:10 +0300

    debog

--- js/java.js
+++ js/java.js
@@ -14,14 +14,31 @@ cacher.onclick  =  function() {
 h.style.visibility='collapse';//hidden,collapse,inherit
 //h.style.display='none';
 }*/
+//var f = document.getElementById("search");
+//form-recherche.addEvenListener("submit",function(){
+	form_recherche.onsubmit = function(e) {
+		e.preventDefault();
 
-form.addEvenListener("submit",function(e){
-	var mot = document.forms.elements["recherche"].value;
-	var sp =/\s*;\s*/;
-	var cle = mot.split(sp);
-	//for(var i=0;i=<cle.length;i++){
-	//		return cle[i];
-	document.location.href="https://www.google.fr/search?q=cle";
-	//}
+		var mot = document.forms["form_recherche"]["recherche"].value;
+		var cle = mot.split(" ");
+		var m = "";
+		var alf ="";
+		//redir_pag= "https://www.google.fr/#q="
+		if (mot ==""){
+			alert("champ vide");
+		}else{
+			for(var i =0;i<=cle.length;i++){
+				m += cle[i]+"+";
+				alf = m.slice(0,(m.length)-1);
+			}
+		//console.log(alf);	
+		document.location.href="https://www.google.fr/#q="+alf;
+		//document.location.assign(`https://www.google.fr/#q=${m}`);
+			
+			
+		}
+	//	console.log(m);
+	//	console.log(alf);	
+	}
+	
 
-})
